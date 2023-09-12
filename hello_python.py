238ef9b5a51d89982525e77354ce25e0b1cf2801
gorodog <rhrnak099gka@gmail.com>
2023-09-12 14:27:20 +0900

    commit message

--- hello_python.py
+++ hello_python.py
@@ -1,8 +1,8 @@
 def print_hello():
     print("Hello World!!")
-
-if __name__ == '__main__':
-	print_hello()
  
 def print_hi():
     print("Hi, my name is Bye!!")
+    
+if __name__ == '__main__':
+	print_hello()
